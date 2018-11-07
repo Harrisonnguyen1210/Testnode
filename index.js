b18e137d70ef323b75340ac71549e2e19ae5208f
Harrisonnguyen1210 <giahungftu2@gmail.com>
2018-11-07 14:36:10 +0200

    "modify index.js"

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-'use strict'
+'use strict';
 
-console.log('Hello world')
+console.log('Hello world');
 
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/a.html")
-})
-app.get('/ditmemay', (req, res) => {
-    res.send("ditmemay")
-})
+});
 
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    res.send(`ditmemay so ${id}`);
-})
 
-app.listen(3000)
+app.listen(3000);
